cf61c6f52a8a456780b6526ad1386fdd8481f651
mahesh577-dell <69102807+mahesh577-dell@users.noreply.github.com>
2026-07-16 06:45:32 +0530

    Update outputs.tf

--- environments/india/tms-dev/outputs.tf
+++ environments/india/tms-dev/outputs.tf
@@ -11,11 +11,11 @@ output "db_password_secret" {
   value       = module.db_secret.secret_id
 }
 
-output "gke_cluster_name" {
+/*output "gke_cluster_name" {
   value = module.gke.cluster_name
-}
+}*/
 
-output "gke_get_credentials_command" {
+/*output "gke_get_credentials_command" {
   description = "Run this to point kubectl at the new cluster"
   value       = module.gke.get_credentials_command
-}
+}*/
